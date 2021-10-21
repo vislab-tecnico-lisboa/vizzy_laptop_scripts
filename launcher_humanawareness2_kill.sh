a55969d80fa850975ce1f9a8ba68e66b3e327327
João Avelino <joao.manuel.avelino@gmail.com>
2021-10-21 12:49:27 +0100

    Launching humanawareness2 on vizzybrain through vizzy-desktop

--- launcher_humanawareness2_kill.sh
+++ launcher_humanawareness2_kill.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh -t vizzy@vizzybrain.local "tmux send-keys -t humanawareness2 C-c"
-ssh -t vizzy@vizzybrain.local "tmux kill-session -t humanawareness2"
+ssh -t vizzy@vizzy-desktop.local "ssh -t vizzy@vizzybrain.local 'tmux send-keys -t humanawareness2 C-c'"
+ssh -t vizzy@vizzy-desktop.local "ssh -t vizzy@vizzybrain.local 'tmux kill-session -t humanawareness2'"
