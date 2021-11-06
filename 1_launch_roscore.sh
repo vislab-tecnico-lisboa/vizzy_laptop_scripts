94135dd2100749382366959d3fa486415a1286e9
João Avelino <joao.avelino@tecnico.ulisboa.pt>
2021-11-06 13:45:43 +0000

    Add yarpserver

--- 1_launch_roscore.sh
+++ 1_launch_roscore.sh
@@ -3,5 +3,12 @@
 ssh -t vizzy@vizzy-desktop.local "tmux new-session -d -s vizzy_startup"
 ssh -t vizzy@vizzy-desktop.local "tmux split-window -v"
 ssh -t vizzy@vizzy-desktop.local "tmux split-window -h"
+ssh -t vizzy@vizzy-desktop.local "tmux selectp -t 0"
+ssh -t vizzy@vizzy-desktop.local "tmux split-window -h"
 ssh -t vizzy@vizzy-desktop.local "tmux send-keys -t vizzy_startup.2 'roscore' ENTER"
+
+#Wait 5 seconds for roscore to run and then launch yarpserver
+sleep(5)
+ssh -t vizzy@vizzy-desktop.local "tmux send-keys -t vizzy_startup.3 'yarpserver --ros' ENTER"
+
 ssh -t vizzy@vizzy-desktop.local "tmux attach"
