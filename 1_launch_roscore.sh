3eb309165d30777f3b1b81e6a846ec98fedafb7a
VisLab Tecnico Lisboa <username@users.noreply.github.com>
2021-11-10 11:54:45 +0000

    Minor fix

--- 1_launch_roscore.sh
+++ 1_launch_roscore.sh
@@ -8,7 +8,7 @@ ssh -t vizzy@vizzy-desktop.local "tmux split-window -h"
 ssh -t vizzy@vizzy-desktop.local "tmux send-keys -t vizzy_startup.2 'roscore' ENTER"
 
 #Wait 5 seconds for roscore to run and then launch yarpserver
-sleep(5)
+sleep 5
 ssh -t vizzy@vizzy-desktop.local "tmux send-keys -t vizzy_startup.3 'yarpserver --ros' ENTER"
 
 ssh -t vizzy@vizzy-desktop.local "tmux attach"
